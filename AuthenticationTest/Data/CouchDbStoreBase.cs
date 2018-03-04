7e3551c4e735fd54396104d4679a471bed37de78
Jamie Lord <jamie@lord.technology>
2018-03-04 15:59:05 +0000

    Begin implementation for CouchDb user and role store

---
+++ AuthenticationTest/Data/CouchDbStoreBase.cs
@@ -0,0 +1,34 @@
+﻿using MyCouch;
+using System;
+
+namespace AuthenticationTest.Data
+{
+    public class CouchDbStoreBase : IDisposable
+    {
+        private const string USERNAME = "CommentService";
+        private const string PASSWORD = "CommentService";
+        private const string SERVER = "http://" + USERNAME + ":" + PASSWORD + "@localhost:5984";
+
+        protected readonly MyCouchStore Store;
+
+        public CouchDbStoreBase(string database)
+        {
+            using (var client = new MyCouchServerClient(SERVER))
+            {
+                var d = client.Databases.PutAsync(database).Result;
+
+                if (!client.Databases.HeadAsync(database).Result.IsSuccess)
+                {
+                    throw new Exception($"Database '{database}' not found!");
+                }
+            }
+
+            Store = new MyCouchStore(SERVER, database);
+        }
+
+        public void Dispose()
+        {
+            Store.Dispose();
+        }
+    }
+}
